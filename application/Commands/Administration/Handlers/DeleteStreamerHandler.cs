50abc7de76f898981af0865ab626881920e785e7
Terry <terrybd@gmail.com>
2020-11-22 14:45:29 -0500

    Refactor delete handler for organization

--- application/Commands/Administration/Handlers/DeleteStreamerHandler.cs
+++ application/Commands/Administration/Handlers/DeleteStreamerHandler.cs
@@ -19,25 +19,35 @@ namespace application.Commands.Administration.Handlers
         {
             var streamer = _context.Streamers.Single(s => s.Id == request.Id);
 
-            var platforms = _context.StreamerPlatforms.Where(p => p.StreamerId == request.Id);
+            RemoveAssociatedPlatforms(request);
 
-            var technologies = _context.StreamerTechnologies.Where(p => p.StreamerId == request.Id);
-
-            foreach (var technology in technologies)
-            {
-                _context.Delete(technology);
-            }
-
-            foreach (var platform in platforms)
-            {
-                _context.Delete(platform);
-            }
+            RemoveAssociatedTechnologies(request);
 
             _context.Delete(streamer);
 
             _context.SaveChanges();
 
             return Unit.Task;
+        }
+
+        private void RemoveAssociatedPlatforms(DeleteStreamer request)
+        {
+            var platforms = _context.StreamerPlatforms.Where(p => p.StreamerId == request.Id);
+
+            foreach (var platform in platforms)
+            {
+                _context.Delete(platform);
+            }
+        }
+
+        private void RemoveAssociatedTechnologies(DeleteStreamer request)
+        {
+            var technologies = _context.StreamerTechnologies.Where(p => p.StreamerId == request.Id);
+
+            foreach (var technology in technologies)
+            {
+                _context.Delete(technology);
+            }
         }
     }
 }
