37e4e23573f777c37799d7d65b8a15b6d96d6703
Terry <terrybd@gmail.com>
2020-12-19 16:32:13 -0500

    Added my-profile to list associated streams

---
+++ application/Query/Streamer/GetAssociatedStreamers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using administration.viewmodels;
+using MediatR;
+
+namespace application.Query.Streamer
+{
+    public class GetAssociatedStreamers : IRequest<IEnumerable<StreamerViewModel>>
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ application/Query/Streamer/Handlers/GetAssociatedStreamersHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using administration.viewmodels;
+using core;
+using MediatR;
+
+namespace application.Query.Streamer.Handlers
+{
+    public class GetAssociatedStreamersHandler : IRequestHandler<GetAssociatedStreamers, IEnumerable<StreamerViewModel>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetAssociatedStreamersHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+        public Task<IEnumerable<StreamerViewModel>> Handle(GetAssociatedStreamers request, CancellationToken cancellationToken)
+        {
+            var streams = _context.Streamers.Where(s => s.Email == request.Email);
+
+            var results = from stream in streams
+                          select new StreamerViewModel
+                          {
+                              Id = stream.Id,
+                              Name = stream.Name,
+                              Description = stream.Description,
+                              Platforms = from p in _context.StreamerPlatforms
+                                          where p.StreamerId == stream.Id
+                                          select new PlatformViewModel
+                                          {
+                                              Name = p.Name,
+                                              Url = p.Url
+                                          },
+                              Technologies = from st in _context.StreamerTechnologies
+                                             join a in _context.AvailableTechnologies on st.TechnologyId equals a.Id
+                                             where st.StreamerId == stream.Id
+                                             select a.Name
+                          };
+
+            return Task.FromResult(results.AsEnumerable());
+        }
+    }
+}
