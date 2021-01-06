99b7b2eada02e8241da394b2da1a024349da7850
Terry <terrybd@gmail.com>
2021-01-05 21:11:17 -0500

    Introduced StreamerClaimRequest model
    Added new model and added to context. Created migration for server.

---
+++ core/Migrations/20210106020923_Add_StreamerClaimRequests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace core.Migrations
+{
+    public partial class Add_StreamerClaimRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StreamerClaimRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CurrentEmail = table.Column<string>(nullable: true),
+                    UpdatedEmail = table.Column<string>(nullable: true),
+                    IsApproved = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StreamerClaimRequests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StreamerClaimRequests");
+        }
+    }
+}
---
+++ core/Models/StreamerClaimRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace core.Models
+{
+    public class StreamerClaimRequest
+    {
+        public Guid Id { get; set; }
+        public string CurrentEmail { get; set; }
+        public string UpdatedEmail { get; set; }
+        public bool IsApproved { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
