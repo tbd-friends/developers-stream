4b1b1d8a20668d71500f2c4f5fe457acec25a5b8
Terry <terrybd@gmail.com>
2021-01-09 15:14:28 -0500

    Added Claims Query and UI
    - Added the command/query for getting unapproved claims
    - Refactored the UI to componentize PendingStreamers and Claims
    - Introduced the PendingClaims view

---
+++ application/Query/Administration/GetPendingClaims.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using viewmodels;
+
+namespace application.Query.Administration
+{
+    public class GetPendingClaims : IRequest<IEnumerable<StreamerClaimViewModel>>
+    {
+        
+    }
+}
---
+++ application/Query/Administration/Handlers/GetPendingClaimsHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using core;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using viewmodels;
+
+namespace application.Query.Administration.Handlers
+{
+    public class GetPendingClaimsHandler : IRequestHandler<GetPendingClaims, IEnumerable<StreamerClaimViewModel>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetPendingClaimsHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<StreamerClaimViewModel>> Handle(GetPendingClaims request, CancellationToken cancellationToken)
+        {
+            var pending = from sc in _context.StreamerClaimRequests
+                where !sc.IsApproved
+                select new StreamerClaimViewModel
+                {
+                    Id = sc.Id,
+                    CurrentEmail = sc.CurrentEmail,
+                    UpdatedEmail = sc.UpdatedEmail,
+                    Created = sc.Created
+                };
+
+            return Task.FromResult(pending.AsEnumerable());
+        }
+    }
+}
